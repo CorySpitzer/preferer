7210d7a17978674ebe0392d53e0a8f7d4c9ccc55
Cory Spitzer & Ben Cornelis <student@epicodus.com>
2015-07-13 10:29:20 -0700

    add ajax request for new question form

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,14 @@
 class QuestionsController < ApplicationController
 
   def index
-    
+
+  end
+
+  def new
+    @question = Question.new
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 end
--- spec/features/question_pages_spec.rb
+++ spec/features/question_pages_spec.rb
@@ -3,6 +3,15 @@ require "rails_helper"
 describe "root path" do
   it "displays a button to ask a question" do
     visit root_path
-    page.should have_content("Ask Question")
+    expect(page).to have_content("Ask Question")
+  end
+end
+
+describe 'loading the question form' do
+  it 'loads the question form', js: true do
+    visit root_path
+    expect(page).to have_no_content 'Save'
+    click_on 'Ask Question'
+    expect(page).to have_content 'Option A'
   end
 end
