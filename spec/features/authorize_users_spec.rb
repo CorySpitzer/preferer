11ba2b7259a6cfbeb2ea344ac2c207c751d56b07
Guest User <student@epicodus.com>
2015-07-14 11:49:55 -0700

    Authorize users to ask questions

---
+++ spec/features/authorize_users_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+describe 'the authorization paths' do
+  it 'redirects to the sign in page if a user is not signed in' do
+    visit new_question_path
+    expect(page).to have_content 'You would rather sign in :)'
+  end
+end
